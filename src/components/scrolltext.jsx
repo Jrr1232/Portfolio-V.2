b386eff6673f2af9643f3946573442edac020462
Joaquin Florencio <Jrr232@gmail.com>
2025-07-31 21:05:10 -0400

    Added project list

--- src/components/scrolltext.jsx
+++ src/components/scrolltext.jsx
@@ -3,10 +3,12 @@
 export default function Scrolltext() {
 
     return (
-        <>
-            <div className="scroll-text">PROJECTS PROJECTS PROJECTS PROJECTS PROJECTS </div>
-
-        </>
-    )
-
+        <div className="scroll-container">
+            <div className="scroll-text">
+                PROJECTS PROJECTS PROJECTS PROJECTS PROJECTS&nbsp;&nbsp;&nbsp;{/* spacing */}
+                PROJECTS PROJECTS PROJECTS PROJECTS PROJECTS&nbsp;&nbsp;&nbsp;
+            </div>
+        </div>
+    );
 }
+
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import Scrolltext from "../components/scrolltext";
-
+import Projects from "../components/projects";
 export default function Home() {
     return (
         <div>
             <main>
                 <Scrolltext />
+                <Projects />
             </main>
         </div>
     );
