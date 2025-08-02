ef1372aa23dd10e12cbeaceabe9defe65cf62cf6
Joaquin Florencio <Jrr232@gmail.com>
2025-08-02 01:22:10 -0400

    added server.js

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
-const express = require('express');
-const path = require('path');
-const app = express();
-const port = process.env.PORT || 3001;
+import express from 'express';
+import path from 'path'
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 app.use(express.static(path.join(__dirname, 'dist')));
 
