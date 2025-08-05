35159c1c678880ef5d2149e2d315c95cf33d8b33
Joaquin Florencio <Jrr232@gmail.com>
2025-08-04 22:34:37 -0400

    changed server.js

--- server.js
+++ server.js
@@ -1,25 +1,12 @@
 import express from 'express';
-import path from 'path'
-import { fileURLToPath } from 'url';
-import { dirname } from 'path';
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
 const app = express();
 const port = process.env.PORT || 3001;
 
-app.use(express.static(path.join(__dirname, 'dist')));
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'dist/index.html'));
+app.get('/', (req, res) => {
+    res.send('Hello World');
 });
 
-try {
-    app.listen(port, () => {
-        console.log(`Server listening on port ${port}`);
-    });
-} catch (err) {
-    console.error("Error starting server:", err);
-}
+app.listen(port, () => {
+    console.log(`Server listening on port ${port}`);
+});
