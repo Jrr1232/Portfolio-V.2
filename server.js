40a0a695abcb85823bf1c98b54642c06249fdb6b
Joaquin Florencio <Jrr232@gmail.com>
2025-08-04 22:36:04 -0400

    changed server.js

--- server.js
+++ server.js
@@ -1,10 +1,18 @@
 import express from 'express';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 const app = express();
 const port = process.env.PORT || 3001;
 
-app.get('/', (req, res) => {
-    res.send('Hello World');
+app.use(express.static(path.join(__dirname, 'dist')));
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'dist', 'index.html'));
 });
 
 app.listen(port, () => {
